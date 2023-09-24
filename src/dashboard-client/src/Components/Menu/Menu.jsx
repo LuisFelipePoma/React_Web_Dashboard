40e1378ea2ee120094f7d40df68641d5e814f8e9
LuisFelipePoma <u202110902@upc.edu.pe>
2023-09-24 12:02:37 -0500

    Initial Commit create-app and menu app

---
+++ src/dashboard-client/src/Components/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Menu.css'
+import Logo from "../../assets/logo.webp"
+
+function Menu() {
+    return (
+        <menu>
+            <img src={Logo} alt=''></img>
+        </menu>
+    )
+}
+
+export default Menu
