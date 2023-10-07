91d6769cb3ba0ce217339d33a1fbf0b3a65c9acb
LuisFelipePoma <u202110902@upc.edu.pe>
2023-10-07 01:37:02 -0500

    header add, still missing font and sizes

--- src/dashboard-vite/src/Components/Header/Header.jsx
+++ src/dashboard-vite/src/Components/Header/Header.jsx
@@ -1,9 +1,25 @@
 import './Header.css'
+import logo from '../../assets/logo.jpeg'
+import { FaSearchengin, FaBell } from 'react-icons/fa'
 
 function Header () {
   return (
     <header className='header-grid'>
-      <p>Header</p>
+      <div className='header-search'>
+        <i>
+          <FaSearchengin />
+        </i>
+        <input type='text' placeholder='Search item, collections and more' />
+      </div>
+      <div className='header-profile'>
+        <i className='header-profile-notify'>
+          <FaBell />
+        </i>
+        <div className='header-profile-img'>
+          <img src={logo} alt='profile image' />
+        </div>
+        <p className='header-profile-name'>Don Gato</p>
+      </div>
     </header>
   )
 }
